bca658be567ebbdb0850324778247afc493e0bd7
Francisco Desktop <francisco.cunha@4mm.com.br>
2016-12-19 17:35:00 -0200

    Add: alterei script

--- js/script.js
+++ js/script.js
@@ -91,61 +91,131 @@ function apagaImg() {
 /*chama fantasma grande*/
 
 
+/*back
+ 
+ var g_estadoImagem1;
+ var g_estadoImagem2;
+ function dado(limiteInf, limiteSup) {
+ return parseInt(Math.random() * (limiteSup - limiteInf) + limiteInf + 0.5);
+ }
+ function trocarCarta(posicao) {
+ document.getElementById("carta_" + (posicao + 1)).style.backgroundImage = "url('imagens/carta_" + vetCartasEmbaralhadas[posicao] + ".png')";
+ 
+ if (g_estadoImagem1 == null) {
+ posicaoImg01 = posicao;
+ g_estadoImagem1 = vetCartasEmbaralhadas[posicao];
+ } else {
+ posicaoImg02 = posicao;
+ g_estadoImagem2 = vetCartasEmbaralhadas[posicao];
+ }
+ if (g_estadoImagem1 != null && g_estadoImagem2 != null) {
+ if (g_estadoImagem1 != g_estadoImagem2) {
+ setTimeout(mostraImagemOriginal, 1000);
+ playFail();
+ 
+ } else {
+ document.getElementById("carta_" + (posicaoImg01 + 1)).style.backgroundImage = "url('imagens/carta_" + g_estadoImagem1 + ".png')";
+ document.getElementById("carta_" + (posicaoImg02 + 1)).style.backgroundImage = "url('imagens/carta_" + g_estadoImagem2 + ".png')";
+ g_estadoImagem1 = null;
+ g_estadoImagem2 = null;
+ playSuccess();
+ 
+ }
+ }
+ }
+ 
+ back*/
+
+
+
 /*---------------------------MOSTRA CARTAS/JOGADA---------------------------*/
-var g_sorteado;
-var g_estadoImagem1;
-var g_estadoImagem2;
+
+/*alterado*/
+var g_estadoImagem1 = "carta_0";
+var g_estadoImagem2 = "carta_0";
+
+var posicaoImg01 = 0;
+var posicaoImg02 = 0;
 function dado(limiteInf, limiteSup) {
     return parseInt(Math.random() * (limiteSup - limiteInf) + limiteInf + 0.5);
 }
 function trocarCarta(posicao) {
     document.getElementById("carta_" + (posicao + 1)).style.backgroundImage = "url('imagens/carta_" + vetCartasEmbaralhadas[posicao] + ".png')";
 
-    if (g_estadoImagem1 == null) {
+    if (g_estadoImagem1 === "carta_0" /*&& posicaoImg01 === 0*/) {//primeiro clique (carta), ou seja, g_estadoImagem1 é o estado inicial
         posicaoImg01 = posicao;
         g_estadoImagem1 = vetCartasEmbaralhadas[posicao];
-    } else {
+
+    } else if (g_estadoImagem1 !== g_estadoImagem2) {//segundo clique (carta), ou seja, g_estadoImagem1 é o estado da posicao, então a nova posicao é armazenada em posicaoImg02
         posicaoImg02 = posicao;
         g_estadoImagem2 = vetCartasEmbaralhadas[posicao];
     }
-    if (g_estadoImagem1 != null && g_estadoImagem2 != null) {
-        if (g_estadoImagem1 != g_estadoImagem2) {
-            document.getElementById("carta_" + (posicaoImg01 + 1)).style.backgroundImage = "url('imagens/carta_0.png')";
-            document.getElementById("carta_" + (posicaoImg02 + 1)).style.backgroundImage = "url('imagens/carta_0.png')";
-            g_estadoImagem1 = null;
-            g_estadoImagem2 = null;
+    if (g_estadoImagem1 !== "carta_0" && g_estadoImagem2 !== "carta_0") {
+        if (g_estadoImagem1 !== g_estadoImagem2) {
+            setTimeout(mostraImagemOriginal, 700);
             playFail();
 
         } else {
             document.getElementById("carta_" + (posicaoImg01 + 1)).style.backgroundImage = "url('imagens/carta_" + g_estadoImagem1 + ".png')";
             document.getElementById("carta_" + (posicaoImg02 + 1)).style.backgroundImage = "url('imagens/carta_" + g_estadoImagem2 + ".png')";
-            g_estadoImagem1 = null;
-            g_estadoImagem2 = null;
+            g_estadoImagem1 = "carta_0";
+            g_estadoImagem2 = "carta_0";
+
             playSuccess();
-             
+           contaAcertos();
+
         }
     }
 }
-
-
-
-
-
-function validaPares() {
+/*alterado*/
+function mostraImagemOriginal() {
+    document.getElementById("carta_" + (posicaoImg01 + 1)).style.backgroundImage = "url('imagens/carta_0.png')";
+    document.getElementById("carta_" + (posicaoImg02 + 1)).style.backgroundImage = "url('imagens/carta_0.png')";
+    g_estadoImagem1 = "carta_0";
+    g_estadoImagem2 = "carta_0";
+}
+var acertos = 0;
+var nome;
+function contaAcertos() {
+    if (acertos < 1) {
+        acertos = acertos + 1;
+    } else {
+        setTimeout(finalizaJogo, 1000);
+    }
 
 }
-var vetCartasEmbaralhadas = new Array();
+
+function finalizaJogo() {
+    var nomeUsuario = prompt("Parabéns, você conclui o jogo! Qual o seu nome?");
+    sessionStorage.setItem('userName', nomeUsuario);
+    nome = sessionStorage.getItem('userName');
+    alert(nome);
+    iniciarJogo();
+
+}
+
+/*function contaAcertos(){
+ acertos.splice(0, 1);
+ if(acertos.length <= 0){
+ nomeUsuario = prompt("Qual o seu nome?");
+ alert(nomeUsuario);
+ }
+ }*/
+
+
+
+var vetCartasEmbaralhadas = new Array();//vetor com numeros de 0 a 19
 function embaralharCartas() {
     var vetCartasNaoEmbaralhadas = new Array();
-    for (i = 0; i < 10; i++) {
-        vetCartasNaoEmbaralhadas[i] = i + 1;
+    for (i = 0; i < 10; i++) { //popula o vetor com nros de 1 a 20
+        vetCartasNaoEmbaralhadas[i] = i + 1; //vetor com numeros de 1 a 20
         vetCartasNaoEmbaralhadas[i + 10] = i + 1;
     }
 
     for (i = 0; i < 20; i++) {
-        var aux = dado(0, vetCartasNaoEmbaralhadas.length - 1);
-        vetCartasEmbaralhadas[i] = vetCartasNaoEmbaralhadas[aux];
-        vetCartasNaoEmbaralhadas.splice(aux, 1);
+        var aux = dado(0, vetCartasNaoEmbaralhadas.length - 1);//gera uma nro aleatório entre 0 e o tamanho do vetor menos 1
+        vetCartasEmbaralhadas[i] = vetCartasNaoEmbaralhadas[aux];// atribui um nro aleatório ao vetor vetCartasEmbaralhadas na posição i
+        vetCartasNaoEmbaralhadas.splice(aux, 1);//Remove um item do vetor vetCartasNaoEmbaralhadas na posição aux
     }
 }
 //delete vetor[i];
